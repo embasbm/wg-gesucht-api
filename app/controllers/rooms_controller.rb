ce1ff0736ac686256ef13b3651923e154ffc8cda
Emba Moussa <emba.moussa@eventinc.de>
2017-08-12 20:10:29 +0200

    call to service

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,7 @@
+# => RoomsController
 class RoomsController < ApplicationController
   def search
-    rooms = WgGesuchtService.new
-    render status: 200, json: {}
+    rooms_count = WgGesuchtService.new.amount_rooms
+    render status: 200, json: rooms_count.to_json
   end
 end
